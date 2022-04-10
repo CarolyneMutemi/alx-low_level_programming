f3521a68d84675ba534be708ad4193f2553f4b15
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-10 11:01:17 -0700

    Project 0x00. C - Hello, World tasks.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main()
+{
+	int TypeInt;
+	char TypeChar;
+	long int TypeLongInt;
+	long long int TypeLongLongInt;
+	float TypeFloat;
+
+	printf("Size of char : %zu byte(s).\n",sizeof(TypeChar));
+	printf("Size of an int : %zu byte(s).\n", sizeof(TypeInt));
+	printf("Size of a long int : %zu byte(s).\n", sizeof(TypeLongInt));
+	printf("Size of a long long int : %zu byte(s).\n", sizeof(TypeLongLongInt));
+	printf("Size of float : %zu byte(s).\n", sizeof(TypeFloat));
+
+	return 0;
+
+}
