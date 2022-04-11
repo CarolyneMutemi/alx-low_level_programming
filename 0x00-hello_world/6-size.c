5576bf3b9c4e384f01aecb65810491806805178c
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-11 02:07:02 -0700

    Changes.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of char: %lu byte(s)\n", sizeof(TypeChar));
 printf("Size of an int: %lu byte(s)\n", sizeof(TypeInt));
 printf("Size of a long int: %lu byte(s)\n", sizeof(TypeLongInt));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(TypeLongLongInt));
-printf("Size of float: %lu byte(s)\n", sizeof(TypeFloat));
+printf("Size of a float: %lu byte(s)\n", sizeof(TypeFloat));
 return (0);
 }
