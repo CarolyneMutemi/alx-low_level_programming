7c71d9fdbd835d04cace83835ddef19e1fd47521
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-11 01:42:37 -0700

    Changes.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 # include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int TypeInt;
-	char TypeChar;
-	long int TypeLongInt;
-	long long int TypeLongLongInt;
-	float TypeFloat;
-
-	printf("Size of char : %lu byte(s).\n", (unsigned long)sizeof(TypeChar));
-	printf("Size of an int : %lu byte(s).\n", (unsigned long)sizeof(TypeInt));
-	printf("Size of a long int :l%zu byte(s).\n", (unsigned long)sizeof(TypeLongInt));
-	printf("Size of a long long int : %lu byte(s).\n", (unsigned long)sizeof(TypeLongLongInt));
-	printf("Size of float : %lu byte(s).\n", (unsigned long)sizeof(TypeFloat));
-
-	return (0);
-
+int TypeInt;
+char TypeChar;
+long int TypeLongInt;
+long long int TypeLongLongInt;
+float TypeFloat;
+printf("Size of char : %lu byte(s).\n", (unsigned long)sizeof(TypeChar));
+printf("Size of an int : %lu byte(s).\n", (unsigned long)sizeof(TypeInt));
+printf("Size of a long int :%lu byte(s).\n", (unsigned long)sizeof(TypeLongInt));
+printf("Size of a long long int : %lu byte(s).\n", (unsigned long)sizeof(TypeLongLongInt));
+printf("Size of float : %lu byte(s).\n", (unsigned long)sizeof(TypeFloat));
+return (0);
 }
