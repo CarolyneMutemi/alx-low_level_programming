a3d40acbad62432b64fd46c9ac9a0cc3cb875abc
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-11 10:21:09 -0700

    0x01. C - Variables, if, else, while tasks.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	int i;
+	char hex;
+
+	for(i = '0'; i <= '9'; i++)
+		putchar(i);
+	for(hex = 'a'; hex <= 'f'; hex++)
+		putchar(hex);
+	putchar('\n');
+
+	return (0);
+}
