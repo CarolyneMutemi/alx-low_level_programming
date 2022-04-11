4ada50eb92f1ea9e8b886301101032c2efee1739
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-11 01:49:32 -0700

    Changes.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char TypeChar;
 long int TypeLongInt;
 long long int TypeLongLongInt;
 float TypeFloat;
-printf("Size of char : %lu byte(s).\n", (unsigned long)sizeof(TypeChar));
-printf("Size of an int : %lu byte(s).\n", (unsigned long)sizeof(TypeInt));
-printf("Size of a long int :%lu byte(s).\n", (unsigned long)sizeof(TypeLongInt));
-printf("Size of a long long int : %lu byte(s).\n", (unsigned long)sizeof(TypeLongLongInt));
-printf("Size of float : %lu byte(s).\n", (unsigned long)sizeof(TypeFloat));
+printf("Size of char: %lu byte(s).\n", (unsigned long)sizeof(TypeChar));
+printf("Size of an int: %lu byte(s).\n", (unsigned long)sizeof(TypeInt));
+printf("Size of a long int:%lu byte(s).\n", (unsigned long)sizeof(TypeLongInt));
+printf("Size of a long long int: %lu byte(s).\n", (unsigned long)sizeof(TypeLongLongInt));
+printf("Size of float: %lu byte(s).\n", (unsigned long)sizeof(TypeFloat));
 return (0);
 }
