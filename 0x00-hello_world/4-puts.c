ce3fb813c42c889d78b705e5e8ede57a8322cfe6
Carolyne <carolmkaysmamba14@gmail.com>
2022-04-11 01:19:00 -0700

    Changes.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - entry point
- *
- * return: always 0 if successful.
- */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
