5b31ce354ba51ef1623ed29b274c34f2276d8f15
Carolyne Mutemi <carolmkaysmamba14@gmail.com>
2022-04-18 22:02:24 +0300

    Change.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,10 @@
 
 /**
  * mul - input
- *
- * Description: multiplies two integers.
- * @a: character to be multiplied with.
- * @b: character to be multiplied with.
- * Return: the result.
+ * Description: multiplies two integers
+ * @a: character to be multiplied with
+ * @b: character to be multiplied with
+ * Return: the result
  */
 
 int mul(int a, int b)
