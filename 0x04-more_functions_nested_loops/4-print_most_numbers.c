1ea00e009081b3cbe26243ed062221ec7a85974a
Carolyne Mutemi <carolmkaysmamba14@gmail.com>
2022-04-18 22:17:15 +0300

    Change.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,13 @@
  *
  * Description: prints the numbers, from 0 to 9, followed by a new line.
  *
- * Return: void.
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int num;
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		if(num == '2' || num == '4')
