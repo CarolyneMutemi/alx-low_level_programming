226af85aecaddbf7f02e722936d52ad96c79e3f1
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-06-16 00:49:31 +0300

    Null

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i = 0;
-char *arr = malloc(nmemb * size);
+char *arr;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
+
+arr = malloc(nmemb * size);
 
 if (arr == NULL)
 return (NULL);
