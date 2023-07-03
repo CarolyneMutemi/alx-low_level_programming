22831e08d4c63d6fe872feec0c5408a60239c3d1
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-07-04 01:40:54 +0300

    Betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,12 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 const list_t *ptr = NULL;
+
 if (h == NULL)
 {
 return (0);
 }
+
 ptr = h;
 
 while (ptr != NULL)
@@ -29,5 +31,6 @@ printf("[%d] %s\n", ptr->len, ptr->str);
 ptr = ptr->next;
 count++;
 }
+
 return (count);
 }
