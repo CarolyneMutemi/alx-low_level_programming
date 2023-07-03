76f82cfe09c99b066fe722d9f9dcc657488546f7
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-07-04 01:24:38 +0300

    Free

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,15 +11,23 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *temp = NULL;
 list_t *ptr = malloc(sizeof(list_t));
-if (ptr == NULL)
+if (!ptr)
 {
 return (NULL);
+free(ptr);
+ptr = NULL;
 exit(1);
 }
 
 ptr->str = strdup(str);
 ptr->len = strlen(ptr->str);
 ptr->next = NULL;
+
+if (!ptr->str)
+{
+    free(ptr->str);
+    ptr->str = NULL;
+}
 
 if (*head == NULL)
 {
