035c1a68406a43d789b51240be11e4e8cb1ece42
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-07-05 02:44:07 +0300

    NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,11 @@ void free_listint2(listint_t **head)
 {
 listint_t *temp = NULL;
 
-if (*head == NULL)
-{}
+if (!*head)
+{
+    free(*head);
+    *head = NULL;
+}
 temp = *head;
 
 while (temp != NULL)
@@ -19,5 +22,4 @@ temp = temp->next;
 free(*head);
 *head = temp;
 }
-*head = NULL;
 }
