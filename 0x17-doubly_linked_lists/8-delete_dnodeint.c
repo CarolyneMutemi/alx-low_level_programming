b358f27298950a366ed958fb88b2b6c3474269c8
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-18 01:39:15 +0300

    Pre

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,6 +17,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
+		if (ptr->next)
+			ptr->next->prev = NULL;
 		*head = ptr->next;
 		free(ptr);
 		return (1);
