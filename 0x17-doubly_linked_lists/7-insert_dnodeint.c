eab1ad799a42c8ec92281fcb61a8dc0d6930e318
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-18 03:35:37 +0300

    Betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,8 +3,7 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: pointer to the head pointer of the list.
- * @idx: index of the list where the new node should be added.
- * Index starts at 0
+ * @idx: index to insert node, starts at 0.
  * @n: new node's data.
  * Return: address of the new node, or NULL if it failed.
  */
@@ -17,25 +16,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	if (*h == NULL)
 		return (NULL);
-
 	while (ptr->next && num != idx)
 	{
 		ptr = ptr->next;
 		num++;
 	}
 	num++;
-
 	if (idx == num && !ptr->next)
 		return (add_dnodeint_end(h, n));
 	if (idx > num && !ptr->next)
 		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 	{
@@ -45,12 +39,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = NULL;
-
 	ptr->prev->next = new;
 	new->prev = ptr->prev;
 	new->next = ptr;
 	ptr->prev = new;
-
 	return (new);
-
 }
